3a308440d0b271262608c879a921eeee341fbd46
zhiiw <1302593554@qq.com>
2020-08-16 16:47:30 +0800

    chapter1

---
+++ 1/HelloworldGra.java
@@ -0,0 +1,29 @@
+/* *****************************************************************************
+ *  Name:              Alan Turing
+ *  Coursera User ID:  123456
+ *  Last modified:     1/1/2019
+ **************************************************************************** */
+
+public class HelloworldGra {
+    public static void main(String[] args) {
+        int imageWidth = 256;
+        int imageHeight = 256;
+
+        System.out.printf("P3\n");
+        System.out.printf("%d %d\n", imageHeight, imageWidth);
+        System.out.printf("255\n");
+        for (int j = imageWidth - 1; j >= 0; j--) {
+            for (int i = 0; i < imageHeight; i++) {
+                double r = (double) i / (imageHeight - 1);
+                double g = (double) j / (imageWidth - 1);
+                double b = 0.25;
+
+                int ir = (int) (255.999 * r);
+                int ig = (int) (255.999 * g);
+                int ib = (int) (255.999 * b);
+                System.out.printf("%d %d %d\n", ir, ig, ib);
+            }
+        }
+
+    }
+}
